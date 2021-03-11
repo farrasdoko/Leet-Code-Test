7f7c1c01608d9a1ebda170b338d76f5f35b4b930
farrasdoko <farras.abiyyu.doko@gmail.com>
2021-03-11 14:34:45 +0700

    fix zero solution

--- Two Sum.playground/Contents.swift
+++ Two Sum.playground/Contents.swift
@@ -5,7 +5,7 @@ class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         var value = Array<Int>()
         outer: for (index, input) in nums.enumerated() {
-            guard input < target else {continue}
+            guard input <= target else {continue}
             for (rootIndex, number) in nums.enumerated() {
                 guard index != rootIndex  else { continue }
                 let result = input + number
@@ -26,3 +26,4 @@ class Solution {
 }
 let solution = Solution()
 print(solution.twoSum([7,2,14,18], 9))
+print(solution.twoSum([0,9,2,4,0], 0))
