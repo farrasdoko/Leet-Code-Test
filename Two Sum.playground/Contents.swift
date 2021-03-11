d2a583ce5e7a4e73882c61b91b137790c9f02c37
farrasdoko <farras.abiyyu.doko@gmail.com>
2021-03-11 14:51:26 +0700

    fix add two numbers for integer

--- Two Sum.playground/Contents.swift
+++ Two Sum.playground/Contents.swift
@@ -5,15 +5,15 @@ class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         var value = Array<Int>()
         outer: for (index, input) in nums.enumerated() {
-            guard input <= target else {continue}
+//            guard input <= target else {continue}
             for (rootIndex, number) in nums.enumerated() {
                 guard index != rootIndex  else { continue }
                 let result = input + number
-                if (number == 0) && (input == 0) {
-                    value.append(index)
-                    value.append(rootIndex)
-                    break outer
-                }
+//                if (number == 0) && (input == 0) {
+//                    value.append(index)
+//                    value.append(rootIndex)
+//                    break outer
+//                }
                 if result == target {
                     value.append(index)
                     value.append(rootIndex)
@@ -27,3 +27,4 @@ class Solution {
 let solution = Solution()
 print(solution.twoSum([7,2,14,18], 9))
 print(solution.twoSum([0,9,2,4,0], 0))
+print (solution.twoSum([-1,-2,-3,-4,-5], -8))
