df2e1a2e6a9474adfa3c5ec1d9beb73fa2f40a76
farrasdoko <farras.abiyyu.doko@gmail.com>
2021-03-11 14:33:37 +0700

    two number solution

---
+++ Two Sum.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var value = Array<Int>()
+        outer: for (index, input) in nums.enumerated() {
+            guard input < target else {continue}
+            for (rootIndex, number) in nums.enumerated() {
+                guard index != rootIndex  else { continue }
+                let result = input + number
+                if (number == 0) && (input == 0) {
+                    value.append(index)
+                    value.append(rootIndex)
+                    break outer
+                }
+                if result == target {
+                    value.append(index)
+                    value.append(rootIndex)
+                    break outer
+                }
+            }
+        }
+        return value
+    }
+}
+let solution = Solution()
+print(solution.twoSum([7,2,14,18], 9))
