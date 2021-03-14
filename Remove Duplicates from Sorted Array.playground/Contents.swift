b3a284d22413dfd0d7aa3daf8c13f307d201528a
farrasdoko <farras.abiyyu.doko@gmail.com>
2021-03-14 13:06:39 +0700

    convert from the solution tab to swift

---
+++ Remove Duplicates from Sorted Array.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+// note: this solution is from the Leet Code solution.
+// I currently just moving the language from java to swift.
+
+class Solution {
+    func removeDuplicates(_ nums: inout [Int]) -> Int {
+        if nums.count == 0 {return 0}
+        var i = 0
+        for j in 1..<nums.count {
+            if nums[j] != nums[i] {
+                i += 1
+                nums[i] = nums[j]
+            }
+        }
+        return i + 1
+    }
+}
